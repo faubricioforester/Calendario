5a935c42e9a61bedcb0319dd17d2f368a906005f
Faubricio Forester <fkahuman@gmail.com>
2017-09-26 13:34:58 -0600

    Implementación de validación de fecha y año bisiesto

--- Calendario.py
+++ Calendario.py
@@ -1,24 +1,40 @@
-print("Por")
-limites_mensuales = {1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
+
+LIMITES_MENSUALES = {1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
+VIGENCIA_CALENDARIO_GREGORIANO = 1582
 
 #*Dado un año perteneciente al rango permitido, determinar si este es bisiesto. El
 #resultado debe ser un valor booleano, True o False.
-def bisiesto(anio):
 
-    return (anio % 4 == 0) and (anio % 100 == 0) or (anio % 400 == 0)
+def bisiesto(año):
+
+    return (año % 4 == 0) and (año % 100 == 0) or (año % 400 == 0)
 
 #Dada una fecha, determinar si ésta es válida. El resultado debe ser un
 #valor booleano, True o False.
 def fecha_es_valida(fecha):
+    dia = fecha[3]
+    mes = fecha[2]
+    año = fecha[1]
+    if año >= VIGENCIA_CALENDARIO_GREGORIANO and mes <= 12:
+        if bisiesto(año) and mes == 2:
+            return dia <= 29
+        else:
+            return dia <= LIMITES_MENSUALES[mes]
 
-
-    return True
 
 #Dada una fecha válida, determinar la fecha del día siguiente. El resultado
 #debe ser una fecha válida (tupla de tres números enteros positivos que corresponde a una fecha
 #en el Calendario gregoriano.
 
 def dia_siguiente(fecha):
+    if fecha_es_valida(fecha):
+        dias = fecha[3]
+        mes = fecha[2]
+        año = fecha[1]
+        
+
+    else:
+        print("La fecha ingresada no es valida")
 
 
     return True
@@ -29,7 +45,10 @@ def dia_siguiente(fecha):
 #primero de enero y el primero de enero, dentro de un mismo año, es 0). El resultado debe ser un
 #número entero.
 #
-def dias_desde_primero_enero():
+def dias_desde_primero_enero(fecha):
+    dias = fecha[3]
+    mes = fecha[2]
+    año = fecha[1]
 
     return True
 
@@ -37,7 +56,7 @@ def dias_desde_primero_enero():
 #semana que le corresponde, con la siguiente codificación: 0 = domingo, 1 = lunes, 2 = martes, 3 =
 #miércoles, 4 = jueves, 5 = viernes, 6 = sábado. El resultado debe ser un número entero, conforme
 #a la codificación indicada.
-def dia_primero_enero():
+def dia_primero_enero(año):
 
     return 0
 
