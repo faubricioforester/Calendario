a6e964243b62dfc3057a14cb98be1803dd018f5b
Faubricio Forester <faubricioforester@gmail.com>
2017-09-26 19:48:04 -0600

    Update CalendarioTest.py

--- CalendarioTest.py
+++ CalendarioTest.py
@@ -4,3 +4,14 @@ pruebasBisiesto=[2000,2001,2096,2100,2200]
 
 for x in pruebasBisiesto:
     print("Año "+ str(x) + " es Bisiesto? " + str(bisiesto(x)))
+    
+pruebasEsFechaValida =[]
+
+for y in pruebasEsFechaValida:
+    print("La fecha: " + str(y[2]) +"/" + str(y[1]) + "/" + str(y[0]) + " es valida?" + str(fechaValida(y)))
+
+
+pruebasDiaSiguiente = []
+
+for y in pruebasDiaSiguiente:
+    print("El dia siguiente a "+ str(y[2]) +"/" + str(y[1]) + "/" + str(y[0])+ " es: " + str(diaSiguiente(y)[2] ) +"/"+ str(diaSiguiente(y)[1] + "/" + str(diaSiguiente(y)[0] ))
